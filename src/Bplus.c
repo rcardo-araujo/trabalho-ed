bf1ab1c2b3479e571b1b3c7a7830f084ddd06d89
igor-alves1 <ig_rodrigues@id.uff.br>
2024-06-20 23:59:53 -0300

    ADD verificaSelecao(FILE *fp) responsável por verificar como se em uma linha possui o nome de uma seleção ou não, sem prejudicar o ponteiro do arquivo]

--- src/Bplus.c
+++ src/Bplus.c
@@ -16,8 +16,11 @@ typedef struct jogador{
     char pais[12];
 } TJ;
 
-TJ *leJogador(FILE *fp){
+TJ *leJogador(FILE *fp, char *selecao){
     TJ *j = (TJ *) malloc(sizeof(TJ));
+    printf("a selecao: %s\n", selecao);
+    strcpy(j->pais, selecao);
+    printf("%s\n", j->pais);
     char c;
     char placeholder[40];
     char dia[3], mes[10], *ano = (char *)malloc(sizeof(char)*5);
@@ -50,6 +53,16 @@ TJ *leJogador(FILE *fp){
     fscanf(fp, "%24[^\n]", j->time);
     fscanf(fp, "%c", &c);
     return j;
+}
+
+int verificaSelecao(FILE *fp){
+    char c;
+    fread(&c, sizeof(char), 1, fp);
+    fseek(fp, -1*sizeof(char), SEEK_CUR);
+    if(c >= 48 && c <= 57){
+        return 0;
+    }
+    return 1;
 }
 
 typedef struct arvbp {
@@ -145,7 +158,7 @@ TARVBP *TARVBP_busca(TARVBP *a, int elem, int t){
 }
 
 void imprimeJogador(TJ *j){
-    printf("id: %d\nnum: %d \npos: %s\nnome: %s\nnasc: %s\nnum_part: %d\nnum_gols: %d\npais_time: %s\ntime: %s\npais: %s",
+    printf("id: %d\nnum: %d \npos: %s\nnome: %s\nnasc: %s\nnum_part: %d\nnum_gols: %d\npais_time: %s\ntime: %s\npais: %s\n",
     j->id, j->num, j->pos, j->nome, j->data_nasc, j->num_part, j->num_gols, j->pais_time, j->time, j->pais);
 }
 
@@ -188,15 +201,15 @@ int main(void){
     free(a); */
     FILE *fp = fopen("EURO.txt", "r");
     int i;
-    char str[40];
-    readLine(fp, str, &i, '\n');
+    char *str = (char *)malloc(sizeof(char)*40);
     TJ *j;
-    
-    for(int i=0; i < 20; i++){
-        j = leJogador(fp);
+    if(verificaSelecao(fp)){
+        readLine(fp, str, &i, '\n');
+        j = leJogador(fp, str);
     }
     fclose(fp);
     imprimeJogador(j);
     free(j);
+    free(str);
     return 0;
 }
