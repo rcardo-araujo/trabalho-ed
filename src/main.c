6aaa7d8d4981504fb45fa2cf8c5e5365567eb10f
pcbmoreira0 <pcbmoreira@id.uff.br>
2024-07-05 17:56:36 -0300

    Removido catalogo2arv da main

--- src/main.c
+++ src/main.c
@@ -1,24 +1,5 @@
 #include "../headers/includes.h"
 
-TARVBP* catalogo2Arv(char *nomeArq, int t){
-    TARVBP *a = TARVBP_cria(nomeArq, t);
-    FILE *fp = fopen("catalogo.txt", "r");
-    char selecao[40];
-    int size;
-    TJ *j;
-    int i = 6;
-    while(i > 0){
-        if(verificaSelecao(fp)){
-            readLine(fp, selecao, &size, '\n');
-        } else {
-            j = leJogador(fp, selecao);
-            a = TARVBP_insere(a, j, t);
-            i--;
-        }
-    }
-    return a;
-}
-
 int main(void){
     TARVBP *a = catalogo2Arv("root", 2);
     a = TARVBP_retira(a, 131, 2);
