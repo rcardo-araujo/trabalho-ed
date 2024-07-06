7d607be31cfbaebda274956788f9d7e415e672f1
pcbmoreira0 <pcbmoreira@id.uff.br>
2024-07-06 00:01:15 -0300

    Adionada busca por ano e mes

--- headers/operacoes.h
+++ headers/operacoes.h
@@ -5,5 +5,8 @@
 
 TARVBP *BuscaJogadorMaisNovo_1(TARVBP *arv, int t);
 int TIJcmp(const void *p1, const void *p2);
+TARVBP *BuscaJogadorMaisVelho_2(TARVBP *arv, int t);
+TARVBP **BuscaJogadoresPorAno_7(TARVBP *arv, int t, char *ano, int *tam);
+TARVBP **BuscaJogadoresPorMes_8(TARVBP *arv, int t, char *mes, int *tam);
 
 #endif
